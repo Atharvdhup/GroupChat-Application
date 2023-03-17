804fae60443c39f0a56b702c772462888d19133f
Atharvdhup <atharvadhup@gmail.com>
2023-03-17 20:51:20 +0530

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class Constants {
+  static String appId ="1:97107146330:web:ec68aef2c421b1792f8a63";
+  static String apiKey ="AIzaSyCch6XmTc4QX4UhdFlHFsl_7MmHF6PG5oU";
+  static String messagingSenderId ="97107146330";
+  static String projectId ="chatappflutter-ecaae";
+  final primaryColor = const Color(0xFFee7b64);
+
+
+}
